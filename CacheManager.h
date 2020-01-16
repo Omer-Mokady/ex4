3104afdaf63cd36de329572e2c15c0cb45a63417
omer <omer.mm@gmail.cpm>
2020-01-16 18:27:46 +0200

    cacheManagaer changes

--- CacheManager.h
+++ CacheManager.h
@@ -12,33 +12,34 @@
 
 using namespace std;
 
-template <typename T>
+template <typename S>
 class CacheManager {
  protected:
   unsigned int limitSize = 5;
  public:
-  unordered_map<string, typename list<pair<string, T>>::iterator> cacheMap;
-  list<pair<string, T>> cacheList;
-  virtual void insert(string key, T obj) = 0;
-  virtual T get(string key) = 0;
+  unordered_map<string, typename list<pair<string, S>>::iterator> cacheMap;
+//  unordered_map<string, typename list<pair<string, T>>::iterator> cacheMap;
+  list<pair<string, S>> cacheList;
+  virtual void insert(string key, S obj) = 0;
+  virtual S get(string key) = 0;
   virtual bool  checkSolutionExistent(string key) = 0;
-  virtual void  foreach(const function<void(T&)> func) = 0;
+  virtual void  foreach(const function<void(S&)> func) = 0;
 };
 
 
-template <typename T>
-class FileCacheManager : public CacheManager<T> {
+template <typename S>
+class FileCacheManager : public CacheManager<S> {
  private:
 
  public:
   FileCacheManager(int num) {
     this->limitSize = num;
   }
-  void insert(string key, T obj) {
-    typename unordered_map<string,typename list<pair<string,T>>::iterator>::iterator itMap
+  void insert(string key, S obj) {
+    typename unordered_map<string,typename list<pair<string,S>>::iterator>::iterator itMap
         = this->cacheMap.find(key);
     ofstream newFile;
-    typename list<pair<string,T>>::iterator itList;
+    typename list<pair<string,S>>::iterator itList;
     // if the key is not exists in the map
     if(itMap == this->cacheMap.end()) {
       // there is not enough space
@@ -57,34 +58,37 @@ class FileCacheManager : public CacheManager<T> {
     }
     // there is space, just put it on and write
     // insert the object to list
-    this->cacheList.push_front(pair<string, T>(key, obj));
+    this->cacheList.push_front(pair<string, S>(key, obj));
     // insert/update the object to the map
     itList = this->cacheList.begin();
     this->cacheMap[key] = itList;
     // write the object to file
-    newFile.open (T::class_name+key, ios::binary);
+    newFile.open (key, ios::binary);
+//    newFile.open (T::class_name+key, ios::binary);
     if(!newFile) {
       throw "problem! can't open file";
     } else {
-      newFile.write((char *) & obj, sizeof(T));
+      newFile.write((char *) & obj, sizeof(S));
       newFile.close();
     }
   }
 
 
-  T get(string key) {
+  S get(string key) {
     ifstream inFile;
-    T newObj;
-    typename unordered_map<string,typename list<pair<string,T>>::iterator>::iterator itMap
+    S newObj;
+    typename unordered_map<string,typename list<pair<string,S>>::iterator>::iterator itMap
         = this->cacheMap.find(key);
     // if the key is not exists in the map
     if(itMap == this->cacheMap.end()) {
-      inFile.open(T::class_name+key, ios::binary);
+//      inFile.open(T::class_name+key, ios::binary);
+      inFile.open(key, ios::binary);
+
       // check if exist in the files
       if(!inFile) {
         throw "problem! can't open file, maybe file is not exist";
       } else {
-        inFile.read((char *) & newObj, sizeof(T));
+        inFile.read((char *) & newObj, sizeof(S));
         inFile.close();
       }
       // key exist in the map
@@ -97,12 +101,13 @@ class FileCacheManager : public CacheManager<T> {
 
   bool checkSolutionExistent(string key) {
     ifstream inFile;
-    T newObj;
-    typename unordered_map<string,typename list<pair<string,T>>::iterator>::iterator itMap
+    S newObj;
+    typename unordered_map<string,typename list<pair<string,S>>::iterator>::iterator itMap
         = this->cacheMap.find(key);
     // if the key is not exists in the map
     if(itMap == this->cacheMap.end()) {
-      inFile.open(T::class_name+key, ios::binary);
+      inFile.open(key, ios::binary);
+//      inFile.open(T::class_name+key, ios::binary);
       // check if exist in the files
       if(!inFile) {
         return false;
@@ -116,8 +121,8 @@ class FileCacheManager : public CacheManager<T> {
     }
   }
 
-  void foreach(const function<void(T&)> func) {
-    typename list<pair<string,T>>::iterator itList = this->cacheList.begin();
+  void foreach(const function<void(S&)> func) {
+    typename list<pair<string,S>>::iterator itList = this->cacheList.begin();
     while(itList != this->cacheList.end()) {
       func(itList->second);
       advance(itList,1);
