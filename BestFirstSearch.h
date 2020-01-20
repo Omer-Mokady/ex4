884fc4f999a6db9db4b1b5c2fac146fb1425e59f
gal <galseidner@gmail.com>
2020-01-20 17:23:06 +0200

    *CMakeList *State.h *CMakeList *MySerialServer

--- BestFirstSearch.h
+++ BestFirstSearch.h
@@ -1,7 +1,3 @@
-//
-// Created by gal on 18/01/2020.
-//
-
 #ifndef EX4__BESTFIRSTSEARCH_H_
 #define EX4__BESTFIRSTSEARCH_H_
 #include "Searcher.h"
@@ -16,62 +12,76 @@
 #include <iterator>
 #include <list>
 
-
-
-
-
 template<typename T>
 class BestFirstSearch : public Searcher<T> {
  public:
-
   virtual ~BestFirstSearch() {};
-  State<T>* Search(Searchable<T> *s);
+  State<T> *Search(Searchable<T> *s);
  private:
-  bool isInQueue(State<T> *s, set<State<T> *> *tracker);
-  bool isInClosedSet(State<T> *s, set<State<T> *> *closed);
-  void relocateInQueue(State<T> *s, int newCost, priority_queue<State<T> *> *q);
-  priority_queue<State<T> *> *_open = new priority_queue<State<T> *>;
-  set<State<T> *> *_queueTracker = new set<State<T> *>();
+  bool isInQueue(State<T> s, set<State<T>> *tracker);
+//  bool isInQueue(State<T> *s, set<State<T> *> *tracker);
+  bool isInClosedSet(State<T> s, set<State<T>> *closed);
+//  bool isInClosedSet(State<T> *s, set<State<T> *> *closed);
+  void relocateInQueue(State<T> s, int newCost, priority_queue<State<T>> *q);
+//  void relocateInQueue(State<T> *s, int newCost, priority_queue<State<T> *> *q);
+  priority_queue<State<T>> *_open = new priority_queue<State<T>>;
+//  priority_queue<State<T>> *_open = new priority_queue<State<T>>;
+  set<State<T>> *_queueTracker = new set<State<T>>();
+//  set<State<T> *> *_queueTracker = new set<State<T> *>();
 };
 
 template<typename T>
-/**
- * TO DO LIST:
- * create "return back trace" in State.h.
- */
-State<T>* BestFirstSearch<T>::Search(Searchable<T> *s) {
-  _open->push((s->getInitialState()));
-  _queueTracker->insert((s->getInitialState())); //tracker set - use us to find element in the queue in O(1).
-  set<State<T> *> *closed = new set<State<T> *>();
+State<T> *BestFirstSearch<T>::Search(Searchable<T> *s) {
+  _open->push(*(s->getInitialState()));
+//  _open->push((s->getInitialState()));
+  _queueTracker->insert(*(s->getInitialState())); //tracker set - use us to find element in the queue in O(1).
+//  _queueTracker->insert((s->getInitialState())); //tracker set - use us to find element in the queue in O(1).
+  set<State<T>> *closed = new set<State<T>>();
+//  set<State<T> *> *closed = new set<State<T> *>();
   while (_open->size() > 0) {
-    State<T> *n = _open->top();
+    State<T> n = _open->top(); //// TODO - make copy constructor for this state for the prev update.
+//    State<T> *n = _open->top();
     _open->pop();
     _queueTracker->erase(n);
     closed->insert(n);
-    if (s->isGoalState(*n)) {
-      return n;
+    if (s->isGoalState(n)) {
+      return &n;
+//      return n;
     }
-    auto successors = s->getAllPossibleStates(*n);
+    auto successors = s->getAllPossibleStates(n);
+//    auto successors = s->getAllPossibleStates(*n);
     typename list<State<pair<int, int>> *>::iterator it = successors.begin();
     for (it; it != successors.end(); advance(it, 1)) {
 //      State<T> *stateDebugger = (*it);
-      if (!isInClosedSet((*it), closed) && !isInQueue((*it), _queueTracker)) { //not in queue and not in closed set.
-        (*it)->setCameFrom(n); //update that the successor prev is n.
-        (*it)->setPathCost(n->getPathCost() + (*it)->getCost()); //update that the successor path cost is his + so far.
-        _open->push((*it)); //push the successor into the open list.
-        _queueTracker->insert((*it)); //push the successor into the tracker set.
+      if (!isInClosedSet(*(*it), closed) && !isInQueue(*(*it), _queueTracker)) { //not in queue and not in closed set.
+//      if (!isInClosedSet((*it), closed) && !isInQueue((*it), _queueTracker)) { //not in queue and not in closed set.
+        (*it)->setCameFrom(&n); //update that the successor prev is n.
+//        (*it)->setCameFrom(n); //update that the successor prev is n.
+        (*it)->setPathCost(n.getPathCost() + (*it)->getCost()); //update that the successor path cost is his + so far.
+//        (*it)->setPathCost(n->getPathCost() + (*it)->getCost()); //update that the successor path cost is his + so far.
+        _open->push(*(*it)); //push the successor into the open list.
+//        _open->push((*it)); //push the successor into the open list.
+        _queueTracker->insert(*(*it)); //push the successor into the tracker set.
+//        _queueTracker->insert((*it)); //push the successor into the tracker set.
       } else {
-        (*it)->setCameFrom(n);
+
 //        State<T> *stateDebugger2 = (*it);
         int currCost = (*it)->getPathCost();
-        int optionalCost = (*it)->getCost() + (*it)->getCameFrom()->getPathCost();
+        int optionalCost = (*it)->getCost() + n.getPathCost();
+//        int optionalCost = (*it)->getCost() + n->getPathCost();
         if (optionalCost < currCost) {
-          if (!isInQueue((*it), _queueTracker)) {
+          (*it)->setCameFrom(&n);
+//          (*it)->setCameFrom(n);
+          if (!isInQueue(*(*it), _queueTracker)) {
+//          if (!isInQueue((*it), _queueTracker)) {
             (*it)->setPathCost(optionalCost);
-            _open->push((*it));
-            _queueTracker->insert((*it));
+            _open->push(*(*it));
+//            _open->push((*it));
+            _queueTracker->insert(*(*it));
+//            _queueTracker->insert((*it));
           } else {
-            relocateInQueue(((*it)), optionalCost, _open);
+            relocateInQueue((*(*it)), optionalCost, _open);
+//            relocateInQueue(((*it)), optionalCost, _open);
           }
         }
       }
@@ -81,11 +91,11 @@ State<T>* BestFirstSearch<T>::Search(Searchable<T> *s) {
 
 //
 template<typename T>
-bool BestFirstSearch<T>::isInQueue(State<T> *s, set<State<T> *> *tracker) {
-  set<State<T> *> *copy = tracker;
-  typename set<State<T> *>::iterator it = copy->begin();
+bool BestFirstSearch<T>::isInQueue(State<T> s, set<State<T>> *tracker) {
+  set<State<T>> *copy = tracker;
+  typename set<State<T>>::iterator it = copy->begin();
   for (it; it != copy->end(); advance(it, 1)) {
-    if (s->equals(*(*(it)))) {
+    if (s.equals(*(it))) {
       cout << "found it!" << endl;
       return true;
     }
@@ -94,46 +104,35 @@ bool BestFirstSearch<T>::isInQueue(State<T> *s, set<State<T> *> *tracker) {
 //    return true;
 //  }
   return false;
-
-
 }
 
-
-
-
-
-
 template<typename T>
-bool BestFirstSearch<T>::isInClosedSet(State<T> *s, set<State<T> *> *closed) {
-  set<State<T> *> *copy = closed;
-  typename set<State<T> *>::iterator it = copy->begin();
+bool BestFirstSearch<T>::isInClosedSet(State<T> s, set<State<T>> *closed) {
+  set<State<T>> *copy = closed;
+  typename set<State<T>>::iterator it = copy->begin();
   for (it; it != copy->end(); advance(it, 1)) {
-    if (s->equals(*(*(it)))) {
-      cout << "found it!" << endl;
+    if (s.equals(*(it))) {
       return true;
     }
   }
-//  if (closed->find(s) != closed->end()) { // testing this.
-//    return true;
-//  }
   return false;
 }
 
 template<typename T>
-void BestFirstSearch<T>::relocateInQueue(State<T> *s, int newCost, priority_queue<State<T> *> *q) {
-  priority_queue<State<T> *> placeHolder;
-  State<T> *temp = q->top();
+void BestFirstSearch<T>::relocateInQueue(State<T> s, int newCost, priority_queue<State<T>> *q) {
+  priority_queue<State<T>> placeHolder;
+  State<T> temp = q->top();
   q->pop();
-  if (temp->equals(*s)) {
-    temp->setCost(newCost);
+  if (temp.equals(s)) {
+    temp.setCost(newCost);
     q->push(temp);
   } else {
-    while (!temp->equals(*s)) {
+    while (!temp.equals(s)) {
       placeHolder.push(temp);
       temp = q->top();
       q->pop();
     }
-    temp->setCost(newCost);
+    temp.setCost(newCost);
     while (placeHolder.size() > 0) {
       q->push(placeHolder.top());
       placeHolder.pop();
